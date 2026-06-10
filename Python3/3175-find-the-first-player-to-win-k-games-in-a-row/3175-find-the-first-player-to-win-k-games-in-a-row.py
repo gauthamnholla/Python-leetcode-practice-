517cb4f6d2320f327c62aa16e1d4d3d5e0806f69
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-06-10 16:08:34 +0300

    Time: 78 ms (39.13%), Space: 34.1 MB (93.91%) - LeetHub

---
+++ Python3/3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findWinningPlayer(self, skills: List[int], k: int) -> int:
+        n = len(skills)
+        if k >= n:
+            return skills.index(max(skills))
+        
+        queue = deque(range(n))
+        win_count = [0] * n
+        
+        while True:
+            player1 = queue.popleft()
+            player2 = queue.popleft()
+            
+            if skills[player1] > skills[player2]:
+                winner, loser = player1, player2
+            else:
+                winner, loser = player2, player1
+            
+            queue.append(loser)
+            win_count[winner] += 1
+            win_count[loser] = 0
+            
+            if win_count[winner] == k:
+                return winner
+            
+            queue.appendleft(winner)
+
+        

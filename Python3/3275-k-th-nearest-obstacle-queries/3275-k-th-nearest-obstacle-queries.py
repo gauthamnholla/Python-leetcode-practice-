773d00d48db72d55771471d74e7e429d901af931
Gautham N Holla <87031833+gauthamnholla@users.noreply.github.com>
2026-07-22 09:18:11 +0300

    Time: 238 ms (31.87%), Space: 92.9 MB (45.05%) - LeetHub

---
+++ Python3/3275-k-th-nearest-obstacle-queries/3275-k-th-nearest-obstacle-queries.py
@@ -0,0 +1,13 @@
+class Solution:
+    def resultsArray(self, queries: List[List[int]], k: int) -> List[int]:
+        n = len(queries)
+        pq = []
+        res = []
+        for i in range(n):
+            sum_val = abs(queries[i][0]) + abs(queries[i][1])
+            heapq.heappush(pq, -sum_val)
+            if len(pq) > k:
+                heapq.heappop(pq)
+            res.append(-pq[0] if len(pq) == k else -1)
+        return res
+        
